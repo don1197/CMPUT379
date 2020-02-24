e718afed5ca3739ad6a9572485303ee5704eac57
Donald Nguyen <dn2@cs.ualberta.ca>
2020-02-24 16:27:02 -0700

    Final touches

--- as2/src/main.cpp
+++ as2/src/main.cpp
@@ -65,18 +65,26 @@ void initializeThreads(pthread_t threads[], int n)
 
 int main(int argc, char *argv[])
 {
+    printf("Warning, if you are inputing from a file, then the file needs to end in a empty line (EOF), or the program will spin, and execute the last command indefinatly. \n");
+
     // Local Variables
     string inputBuffer;
 
-    int numThreads = (argc >= 2) ? atoi(argv[1]) : 3;
-    int id = (argc >= 3) ? atoi(argv[2]) : 0;
+    int numThreads = (argc >= 2) ? atoi(argv[1]) : 3;   // Default to 3 if none entered
+    int id = (argc >= 3) ? atoi(argv[2]) : 0;           // 
 
     string outputFileName = (id != 0) ? "prodcom." + to_string(id) + ".log" : "prodcom.log";
 
-    if(!freopen( outputFileName.c_str(), "w", stdout ))
+    if(id != 0)
     {
-        printf("Failed to create log file, exiting\n");
-        return 1;
+        printf("Pipeing output to %s instead of terminal. Too see output on terminal remove 2nd argument\n", outputFileName.c_str());
+
+        if(!freopen( outputFileName.c_str(), "w", stdout))
+        {
+            // For no enter, pipe it to screen instead.
+            printf("Failed to create log file, exiting\n");
+            return 1;
+        }
     }
 
     pthread_t threads [numThreads];    
@@ -84,8 +92,13 @@ int main(int argc, char *argv[])
     initializeThreads(threads, numThreads);
     freeThreads++;
     startTime = clock();
-    printf("Timestamp | Thread Number | Items in Queue | Action | Ammount\n");
 
+    if(id == 0)
+    {
+        printf("Start Inputing: (T<N>, S<N>, or Q\n");
+    }
+    
+    printf("Timestamp | Thread Number | Items in Queue | Action | Ammount\n");
     while(1)
     {
         getline(cin, inputBuffer);
@@ -94,6 +107,7 @@ int main(int argc, char *argv[])
         m_freeThreadsAccessor.lock();
         if(cin.eof() && freeThreads == numThreads + 1 && pendingWork.size() == 0)
         {
+            printf("All work completed\n");
             break;
         }
         m_freeThreadsAccessor.unlock();
@@ -135,6 +149,11 @@ int main(int argc, char *argv[])
 
                     printf("%f | ID = 0 | Q = %d | Complete | %d\n", (double) (difftime(clock(), startTime) / CLOCKS_PER_SEC), pendingWork.size(), num);
                 }
+                else if(code == "q" || code == "Q")
+                {
+                    printf("User quit cmd");
+                    break;
+                }
                 else
                 {
                     cerr << "The letter must be either \'T\' or \'S\'" << endl;
@@ -143,7 +162,7 @@ int main(int argc, char *argv[])
         }
     }
     usleep(10); // Ensures printf isn't disjointed   
-    printf("All work finished\nKilling all threads\nExiting");
+    printf("Killing all threads\nExiting");
     
     for(pthread_t i : threads)
     {
